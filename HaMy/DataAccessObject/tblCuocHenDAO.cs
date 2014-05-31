7e4649cafea70a1b41dc1f715618c3e48f0b5d29
thang.991992 <thang.991992@gmail.com>
2014-05-31 20:12:21 +0000

    System Tray

---
+++ HaMy/DataAccessObject/tblCuocHenDAO.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using HaMy.EntityObject;
+
+namespace HaMy.DataAccessObject
+{
+    public class tblCuocHenDAO
+    {
+        #region "CheckExists"
+        /// <summary> 1. CuocHen_CheckExists </summary>
+        /// <param name="_tblCuocHenEO"></param>
+        /// <returns></returns>
+        public static bool CuocHen_CheckExists(tblCuocHenEO _tblCuocHenEO)
+        {
+            using (SqlConnection conn = ConnectionDAO.getConnection())
+            {
+                bool bOutput = false;
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("tblCuocHen_CheckExists", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@PK_lCuocHen", _tblCuocHenEO.PK_lCuocHen));
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        bOutput = Convert.ToBoolean(dr["return_value"]);
+                    }
+                    conn.Close();
+                    return bOutput;
+                }
+                catch (Exception)
+                {
+                    conn.Close();
+                    return bOutput;
+                }
+            }
+        }
+        #endregion
+
+        #region "Insert, Update, Delete"
+        /// <summary> 2. CuocHen_Insert </summary>
+        /// <param name="_tblCuocHenEO"></param>
+        /// <returns></returns>
+        public static bool CuocHen_Insert(tblCuocHenEO _tblCuocHenEO)
+        {
+            using (SqlConnection conn = ConnectionDAO.getConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("tblCuocHen_Insert", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@FK_iNguoiDung", _tblCuocHenEO.FK_iNguoiDung));
+                    cmd.Parameters.Add(new SqlParameter("@FK_iDoiTac", _tblCuocHenEO.FK_iDoiTac));
+                    cmd.Parameters.Add(new SqlParameter("@sNoiDung", _tblCuocHenEO.sNoiDung));
+                    cmd.Parameters.Add(new SqlParameter("@sDiaDiem", _tblCuocHenEO.sDiaDiem));
+                    cmd.Parameters.Add(new SqlParameter("@tNgayGioBatDau", _tblCuocHenEO.tNgayGioBatDau));
+                    cmd.Parameters.Add(new SqlParameter("@tNgayGioKetThuc", _tblCuocHenEO.tNgayGioKetThuc));
+                    cmd.Parameters.Add(new SqlParameter("@iTrangThai", _tblCuocHenEO.iTrangThai));
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    conn.Close();
+                    return false;
+                }
+            }
+        }
+
+        /// <summary> 3. CuocHen_Update </summary>
+        /// <param name="_tblCuocHenEO"></param>
+        /// <returns></returns>
+        public static bool CuocHen_Update(tblCuocHenEO _tblCuocHenEO)
+        {
+            using (SqlConnection conn = ConnectionDAO.getConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("tblCuocHen_Update", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@PK_lCuocHen", _tblCuocHenEO.PK_lCuocHen));
+                    cmd.Parameters.Add(new SqlParameter("@FK_iNguoiDung", _tblCuocHenEO.FK_iNguoiDung));
+                    cmd.Parameters.Add(new SqlParameter("@FK_iDoiTac", _tblCuocHenEO.FK_iDoiTac));
+                    cmd.Parameters.Add(new SqlParameter("@sNoiDung", _tblCuocHenEO.sNoiDung));
+                    cmd.Parameters.Add(new SqlParameter("@sDiaDiem", _tblCuocHenEO.sDiaDiem));
+                    cmd.Parameters.Add(new SqlParameter("@tNgayGioBatDau", _tblCuocHenEO.tNgayGioBatDau));
+                    cmd.Parameters.Add(new SqlParameter("@tNgayGioKetThuc", _tblCuocHenEO.tNgayGioKetThuc));
+                    cmd.Parameters.Add(new SqlParameter("@iTrangThai", _tblCuocHenEO.iTrangThai));
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    conn.Close();
+                    return false;
+                }
+            }
+        }
+
+        /// <summary> 4. CuocHen_Delete </summary>
+        /// <param name="_tblCuocHenEO"></param>
+        /// <returns></returns>
+        public static bool CuocHen_Delete(tblCuocHenEO _tblCuocHenEO)
+        {
+            using (SqlConnection conn = ConnectionDAO.getConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("tblCuocHen_Delete", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@PK_lCuocHen", _tblCuocHenEO.PK_lCuocHen));
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    conn.Close();
+                    return false;
+                }
+            }
+        }
+
+        /// <summary> 5. CuocHen_DeleteList </summary>
+        /// <param name="_ListPK_sMaMonhoc"></param>
+        /// <returns></returns>
+        public static bool CuocHen_DeleteList(String _LisPK_lCuocHen)
+        {
+            using (SqlConnection conn = ConnectionDAO.getConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("tblCuocHen_DeleteList", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@ListPK_lCuocHen", _LisPK_lCuocHen));
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    conn.Close();
+                    return false;
+                }
+            }
+        }
+        #endregion
+
+        #region "Select"
+        /// <summary> 6. CuocHen_SelectItem </summary>
+        /// <param name="_tblCuocHenEO"></param>
+        /// <returns></returns>
+        public static tblCuocHenEO CuocHen_SelectItem(tblCuocHenEO _tblCuocHenEO)
+        {
+            tblCuocHenEO oOutput = new tblCuocHenEO();
+            DataSet ds = null;
+            using (SqlConnection conn = ConnectionDAO.getConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    SqlDataAdapter da = new SqlDataAdapter("tblCuocHen_SelectItem", conn);
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    da.SelectCommand.Parameters.Add(new SqlParameter("@PK_lCuocHen", _tblCuocHenEO.PK_lCuocHen));
+                    ds = new DataSet();
+                    da.Fill(ds);
+                    conn.Close();
+                    oOutput = DataSet2Object.CuocHenDO(ds);
+                    return oOutput;
+                }
+                catch (Exception)
+                {
+                    conn.Close();
+                    return oOutput;
+                }
+            }
+        }
+        
+        /// <summary> 8. CuocHen_SelectList </summary>
+        /// <param name="_tblCuocHenEO"></param>
+        /// <returns></returns>
+        public static DataSet CuocHen_SelectList()
+        {
+            DataSet dsOutput = null;
+            using (SqlConnection conn = ConnectionDAO.getConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    SqlDataAdapter da = new SqlDataAdapter("tblCuocHen_SelectList", conn);
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    dsOutput = new DataSet();
+                    da.Fill(dsOutput);
+                    conn.Close();
+                    return dsOutput;
+                }
+                catch (Exception)
+                {
+                    conn.Close();
+                    return dsOutput;
+                }
+            }
+        }
+
+        /// <summary> 8. CuocHen_SelectList_Notifycation </summary>
+        /// <param name="_tblCuocHenEO"></param>
+        /// <returns></returns>
+        public static DataSet CuocHen_SelectList_Notifycation()
+        {
+            DataSet dsOutput = null;
+            using (SqlConnection conn = ConnectionDAO.getConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    SqlDataAdapter da = new SqlDataAdapter("tblCuocHen_SelectList_Notifycation", conn);
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    dsOutput = new DataSet();
+                    da.Fill(dsOutput);
+                    conn.Close();
+                    return dsOutput;
+                }
+                catch (Exception)
+                {
+                    conn.Close();
+                    return dsOutput;
+                }
+            }
+        }
+
+        /// <summary> 9. CuocHen_Search </summary>
+        /// <param name="_tblCuocHenEO"></param>
+        /// <returns></returns>
+        public static DataSet CuocHen_Search(tblCuocHenEO _tblCuocHenEO)
+        {
+            DataSet dsOutput = null;
+            using (SqlConnection conn = ConnectionDAO.getConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    SqlDataAdapter da = new SqlDataAdapter("tblCuocHen_Search", conn);
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    da.SelectCommand.Parameters.Add(new SqlParameter("@PK_lCuocHen", (_tblCuocHenEO.PK_lCuocHen == 0) ? (object)DBNull.Value : _tblCuocHenEO.PK_lCuocHen));
+                    da.SelectCommand.Parameters.Add(new SqlParameter("@FK_iNguoiDung", (_tblCuocHenEO.FK_iNguoiDung == 0) ? (object)DBNull.Value : _tblCuocHenEO.FK_iNguoiDung));
+                    da.SelectCommand.Parameters.Add(new SqlParameter("@FK_iDoiTac", (_tblCuocHenEO.FK_iDoiTac == 0) ? (object)DBNull.Value : _tblCuocHenEO.FK_iDoiTac));
+                    da.SelectCommand.Parameters.Add(new SqlParameter("@sNoiDung", (String.IsNullOrEmpty(_tblCuocHenEO.sNoiDung)) ? (object)DBNull.Value : _tblCuocHenEO.sNoiDung));
+                    da.SelectCommand.Parameters.Add(new SqlParameter("@sDiaDiem", (String.IsNullOrEmpty(_tblCuocHenEO.sDiaDiem)) ? (object)DBNull.Value : _tblCuocHenEO.sDiaDiem));
+                    da.SelectCommand.Parameters.Add(new SqlParameter("@tNgayGioBatDau", (_tblCuocHenEO.tNgayGioBatDau == DateTime.MinValue) ? (object)DBNull.Value : _tblCuocHenEO.tNgayGioBatDau));
+                    da.SelectCommand.Parameters.Add(new SqlParameter("@tNgayGioKetThuc", (_tblCuocHenEO.tNgayGioKetThuc == DateTime.MinValue) ? (object)DBNull.Value : _tblCuocHenEO.tNgayGioKetThuc));
+                    da.SelectCommand.Parameters.Add(new SqlParameter("@iTrangThai", (_tblCuocHenEO.iTrangThai == 0) ? (object)DBNull.Value : _tblCuocHenEO.iTrangThai));
+                    dsOutput = new DataSet();
+                    da.Fill(dsOutput);
+                    conn.Close();
+                    return dsOutput;
+                }
+                catch (Exception)
+                {
+                    conn.Close();
+                    return dsOutput;
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ HaMy/frmMoiQuanHe.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using HaMy.EntityObject;
+using HaMy.SharedLibraries;
+using HaBa.SharedLibraries;
+using HaMy.DataAccessObject;
+
+namespace HaMy
+{
+    public partial class frmMoiQuanHe : Form
+    {
+        #region "Form"
+        public frmMoiQuanHe()
+        {
+            InitializeComponent();
+        }
+        
+        private void frmMoiQuanHe_Load(object sender, EventArgs e)
+        {
+            ClearMessages();
+            txtPK_iMoiQuanHe.Enabled = false;
+            BindDataGridView();
+        }
+        #endregion
+
+        #region LoadData
+        public void BindDataGridView()
+        {
+            grvMoiQuanHe.Visible = false;
+            DataSet dsMoiQuanHe = new DataSet();
+            try
+            {
+                dsMoiQuanHe = tblMoiQuanHeDAO.MoiQuanHe_SelectList();
+                if (dsMoiQuanHe.Tables[0].Rows.Count > 0)
+                {
+                    grvMoiQuanHe.Visible = true;
+                    grvMoiQuanHe.AutoGenerateColumns = false;
+                    grvMoiQuanHe.DataSource = dsMoiQuanHe.Tables[0];
+                    //grvMoiQuanHe.DataMember = dsMoiQuanHe.Tables[0].ToString();
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        public void BindDataDetail(tblMoiQuanHeEO _tblMoiQuanHeEO)
+        {
+            txtPK_iMoiQuanHe.Text = Convert.ToString(_tblMoiQuanHeEO.PK_iMoiQuanHe);
+            txtsTen.Text = Convert.ToString(_tblMoiQuanHeEO.sTen);
+        }
+
+        public tblMoiQuanHeEO getObject()
+        {
+            try
+            {
+                tblMoiQuanHeEO _tblMoiQuanHeEO = new tblMoiQuanHeEO();
+                _tblMoiQuanHeEO.PK_iMoiQuanHe = (String.IsNullOrEmpty(txtPK_iMoiQuanHe.Text)) ? 0 : Convert.ToInt32(txtPK_iMoiQuanHe.Text);
+                _tblMoiQuanHeEO.sTen = Convert.ToString(txtsTen.Text);
+                return _tblMoiQuanHeEO;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+       
+        public void ClearMessages()
+        {
+            //lblMsg.Text = "";
+            lblPK_iMoiQuanHe.Text = "";
+            lblsTen.Text = "";
+        }
+        #endregion
+
+        #region "Event DataGridView"
+        private void grvMoiQuanHe_SelectionChanged(object sender, EventArgs e)
+        {
+            tblMoiQuanHeEO _tblMoiQuanHeEO = new tblMoiQuanHeEO();
+            foreach (DataGridViewRow row in grvMoiQuanHe.SelectedRows)
+            {
+                _tblMoiQuanHeEO.PK_iMoiQuanHe = Convert.ToInt32(row.Cells[0].Value);
+                _tblMoiQuanHeEO.sTen = row.Cells[1].Value.ToString();
+            }
+            BindDataDetail(_tblMoiQuanHeEO);
+        }
+        #endregion
+
+        #region "Event Button"
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataSet dsMoiQuanHe = new DataSet();
+                dsMoiQuanHe = tblMoiQuanHeDAO.MoiQuanHe_Search(getObject());
+                grvMoiQuanHe.Visible = true;
+                grvMoiQuanHe.DataSource = dsMoiQuanHe.Tables[0];
+                //grvMoiQuanHe.DataMember = dsMoiQuanHe.Tables[0].ToString();
+                //grvMoiQuanHe.DataBind();
+            }
+            catch { }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            ClearMessages();
+            lblMsg.Text = "";
+            try
+            {
+                if (tblMoiQuanHeDAO.MoiQuanHe_Insert(getObject()) == true)
+                {
+                    lblMsg.Text = Messages.Them_Thanh_Cong;
+                }
+                else
+                {
+                    lblMsg.Text = Messages.Them_That_Bai;
+                }
+                BindDataGridView();
+                tblMoiQuanHeEO _tblMoiQuanHeEO = new tblMoiQuanHeEO();
+                BindDataDetail(_tblMoiQuanHeEO);
+                ClearMessages();
+            }
+            catch (Exception ex)
+            {
+                lblMsg.Text = Messages.Loi + ex.Message;
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            ClearMessages();
+            lblMsg.Text = "";
+            try
+            {
+                if (tblMoiQuanHeDAO.MoiQuanHe_Update(getObject()) == true)
+                {
+                    lblMsg.Text = Messages.Sua_Thanh_Cong;
+                }
+                else
+                {
+                    lblMsg.Text = Messages.Sua_That_Bai;
+                }
+                BindDataGridView();
+                tblMoiQuanHeEO _tblMoiQuanHeEO = new tblMoiQuanHeEO();
+                BindDataDetail(_tblMoiQuanHeEO);
+            }
+            catch (Exception ex)
+            {
+                lblMsg.Text = Messages.Loi + ex.Message;
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            ClearMessages();
+            lblMsg.Text = "";
+            try
+            {
+                if (tblMoiQuanHeDAO.MoiQuanHe_Delete(getObject()) == true)
+                {
+                    lblMsg.Text = Messages.Xoa_Thanh_Cong;
+                }
+                else
+                {
+                    lblMsg.Text = Messages.Xoa_That_Bai;
+                }
+                BindDataGridView();
+                tblMoiQuanHeEO _tblMoiQuanHeEO = new tblMoiQuanHeEO();
+                BindDataDetail(_tblMoiQuanHeEO);
+            }
+            catch (Exception ex)
+            {
+                lblMsg.Text = Messages.Loi + ex.Message;
+            }
+        }
+
+        private void btnLamMoi_Click(object sender, EventArgs e)
+        {
+            ClearMessages();
+            lblMsg.Text = "";
+            BindDataGridView();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ClearMessages();
+            lblMsg.Text = "";
+            tblMoiQuanHeEO _tblMoiQuanHeEO = new tblMoiQuanHeEO();
+            BindDataDetail(_tblMoiQuanHeEO);
+        }
+        #endregion
+
+
+    }
+}
